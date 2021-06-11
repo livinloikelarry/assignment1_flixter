58c230e41cb3bf9e1252eecbfa10b03921bfdb72
Yuvia Leon <yuvy.leon.yl@gmail.com>
2021-06-11 13:52:37 -0500

    changed the flow of the code which largely stayed the same and added logic to clear a search term and go back to one page of the now playing movies

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ console.log("page loaded");
 // global constants
 // api key needed in url
 const myApi = "dff92858cb542d34fe71442401efe943";
-const pageSize = "9";
 const baseUrl = "https://api.themoviedb.org/3";
 // global variables
 var currentApiPage = 1;
@@ -14,6 +13,7 @@ const searchForm = document.querySelector("#search-form");
 const searchInput = document.querySelector("#search-input");
 const posterArea = document.querySelector(".moviePosters");
 const showMoreBtn = document.querySelector("#show-more-btn");
+const clearBtn = document.querySelector(".clear-btn");
 
 // get search results from API and call displayResults so that
 // they are visible to the user
@@ -31,10 +31,10 @@ async function getResults() {
 
 // this function inserts html so that posters display
 function displayResults(movieData) {
+  console.log("inside of display results.");
   let imagePath = movieData.poster_path;
   urlOfPoster = `https://image.tmdb.org/t/p/w500/${imagePath}`;
   //console.log(movieData); <-- a previous console log to show the key:value pairs
-  //   posterArea.innerHTML += `<img src="${urlOfPoster}" alt="${movieData.title}"></img>`;
   posterArea.innerHTML += `<div class = "individualMovie">
   <img src="${urlOfPoster}" alt="${movieData.title}"></img>
   <div class = "name-rating">
@@ -52,6 +52,7 @@ getResults();
 // /** On form submit, get results and add to list. */
 async function handleFormSubmit(event) {
   event.preventDefault();
+  // clear html
   posterArea.innerHTML = "";
   // to access the value that the user typed in
   currentSearchTerm = searchInput.value;
@@ -60,10 +61,11 @@ async function handleFormSubmit(event) {
   const results = await getSearchResults(currentSearchTerm);
   // resetting the search input to nothing
   searchInput.value = "";
-  currentApiPage++;
+  //currentApiPage++;
 }
 
 async function getSearchResults(searchTerm) {
+  currentApiPage = 1;
   currentSearchTerm = searchTerm;
   console.log("inside of search results search term is:", currentSearchTerm);
   let response = await fetch(
@@ -72,7 +74,7 @@ async function getSearchResults(searchTerm) {
   let jsonResponse = await response.json();
   let responseData = jsonResponse.results;
   console.log(responseData);
-  currentApiPage = 1;
+  //   currentApiPage = 1;
   console.log("inside of search. count is:", currentApiPage);
   // will call function on each element in the array
   responseData.forEach((el) => displayResults(el));
@@ -90,4 +92,19 @@ async function handleShowMoreClick(event) {
   const results = await getResults();
 }
 
+function handleClearBtn() {
+  console.log("THE BUTTON WAS CLICKED");
+  // clear the html
+  posterArea.innerHTML = "";
+  // how many "now playing" pages did the user load?
+  console.log("the current api page count is: ", currentApiPage);
+  let userPageCount = currentApiPage;
+  for (let i = 1; i <= userPageCount; i++) {
+    console.log("inside for loop setting currentApiPage = to:", i);
+    currentApiPage = userPageCount;
+    getResults();
+  }
+}
+
 showMoreBtn.addEventListener("click", handleShowMoreClick);
+clearBtn.addEventListener("click", handleClearBtn);
