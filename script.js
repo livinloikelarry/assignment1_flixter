d3ff0ac9efc2b0fcbc7a12f4c156becd0022ac04
Yuvia Leon <yuvy.leon.yl@gmail.com>
2021-06-11 09:01:12 -0500

    added functionality to the search bar

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const myApi = "dff92858cb542d34fe71442401efe943";
 const pageSize = "9";
 const baseUrl = "https://api.themoviedb.org/3";
 // global variables
-https: var currentApiPage = 0;
+var currentApiPage = 1;
 var currentSearchTerm = "";
 
 //page elements
@@ -15,11 +15,12 @@ const searchInput = document.querySelector("#search-input");
 const posterArea = document.querySelector(".moviePosters");
 const showMoreBtn = document.querySelector("#show-more-btn");
 
-// get search results from API
+// get search results from API and call displayResults so that
+// they are visible to the user
 async function getResults() {
   //const offset = currentApiPage * pageSize;
   let response = await fetch(
-    `https://api.themoviedb.org/3/movie/now_playing?api_key=${myApi}&language=en-US&page=1`
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${myApi}&language=en-US&page=${currentApiPage}`
   );
   let jsonResponse = await response.json();
   let responseData = jsonResponse.results;
@@ -27,10 +28,11 @@ async function getResults() {
   responseData.forEach((el) => displayResults(el));
 }
 
+// this function inserts html so that posters display
 function displayResults(movieData) {
   let imagePath = movieData.poster_path;
   urlOfPoster = `https://image.tmdb.org/t/p/w500/${imagePath}`;
-  console.log(movieData);
+  //console.log(movieData); <-- a previous console log to show the key:value pairs
   //   posterArea.innerHTML += `<img src="${urlOfPoster}" alt="${movieData.title}"></img>`;
   posterArea.innerHTML += `<div class = "individualMovie">
   <img src="${urlOfPoster}" alt="${movieData.title}"></img>
@@ -43,26 +45,42 @@ function displayResults(movieData) {
 </div>`;
 }
 
+// important function call that displays "now playing" movies
 getResults();
 
 // /** On form submit, get results and add to list. */
-// async function handleFormSubmit(event) {
-//   event.preventDefault();
-//   posterArea.innerHTML = "";
-//   currentSearchTerm = searchInput.value;
-//   const results = await getResults(currentSearchTerm);
-//   displayResults(results);
-//   searchInput.value = "";
-//   currentApiPage++;
-//   showMoreBtn.classList.remove("hidden");
-// }
+async function handleFormSubmit(event) {
+  event.preventDefault();
+  posterArea.innerHTML = "";
+  // to access the value that the user typed in
+  currentSearchTerm = searchInput.value;
+  console.log("the word that you searched:", currentSearchTerm);
+  // getting response from API
+  const results = await getSearchResults(currentSearchTerm);
+  // resetting the search input to nothing
+  searchInput.value = "";
+  currentApiPage++;
+}
 
-// searchForm.addEventListener("submit", handleFormSubmit);
+async function getSearchResults(searchTerm) {
+  //const offset = currentApiPage * pageSize;
+  let response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${myApi}&language=en-US&query=${searchTerm}&page=${currentApiPage}&include_adult=false`
+  );
+  let jsonResponse = await response.json();
+  let responseData = jsonResponse.results;
+  console.log(responseData);
+  // will call function on each element in the array
+  responseData.forEach((el) => displayResults(el));
+}
 
-// async function handleShowMeMoreClick(event) {
-//   const results = await getResults(currentSearchTerm);
-//   displayResults(results);
-//   currentApiPage++;
-// }
+searchForm.addEventListener("submit", handleFormSubmit);
 
-// showMoreBtn.addEventListener("click", handleShowMeMoreClick);
+async function handleShowMoreClick(event) {
+  //increase the value of the api page so that more results are shown
+  currentApiPage++;
+  console.log(currentApiPage);
+  const results = await getResults();
+}
+
+showMoreBtn.addEventListener("click", handleShowMoreClick);
